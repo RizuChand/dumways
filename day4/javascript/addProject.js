27656a1c5d235d6683c3630ed43d93507d84c5fe
Reinzchand <dragoriza123@gmail.com>
2023-07-05 23:50:54 +0700

    day 4 hampir selesai

--- day4/javascript/addProject.js
+++ day4/javascript/addProject.js
@@ -12,36 +12,70 @@ let blogData = [];
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
+
     
-   const valueProjectName = projectName.value;
-   const valueStardate = startDate.value;
-   const valueEndDate = endDate.value;
-   const valueDescription = description.value;
-   let files = file.files;
-    
+    const valueProjectName = projectName.value;
+    const valueStardate = startDate.value;
+    const valueEndDate = endDate.value;
+    const valueDescription = description.value;
+    let files = file.files;
+   
    files = URL.createObjectURL(files[0]);
 
-    let data = [];
-    document.querySelectorAll('[type="checkbox"]').forEach(item => {
-        if (item.checked === true) {
-            data.push(item.value);
-        }
+   const iconNodeJS = '<img src="./img/myProject/node-js.svg" alt="nodejs">';
+   const iconNextJS = '<img src="./img/myProject/nextjs.png" alt="nextjs">';
+   const iconReactJS = '<img src="./img/myProject/react.svg" alt="reactjs">';
+   const icontTypescript = '<img src="./img/myProject/typescript.png" alt="typescript">';
 
-    })
-
-
+   
+   // pengambilan data dari checkbox
+   let checkNodeJS = document.querySelector("#nodejs").checked ? iconNodeJS : "";
+   let checkNextJS = document.querySelector("#nextjs").checked ? iconNextJS : "";
+   let checkReactJS = document.querySelector("#reactjs").checked ? iconReactJS : "";
+   let checkTypescript = document.querySelector("#typescript").checked ? icontTypescript : "";
+    
+    
     let schema = {
         valueProjectName,
         valueStardate,
         valueEndDate,
         valueDescription,
-        // files,
-        data
+        files,
     }
+    
+    
+    
+    let parent = document.querySelector("#wrapper-myproject");
+    let h1 = document.createElement("li");
+    
+ 
+    h1.innerHTML = `<div class="item-card">
+    <img src="${files}" alt="">
+    <div class="text">
+        <h3>${valueProjectName}</h3>
+        <p>${valueDescription}</p>
+    </div>
+    <div class="wrapper-checkbox">
+        
+        <div class="checkbox-item">
+        ${checkNodeJS}
+        ${checkNextJS}
+        ${checkReactJS}
+        ${checkTypescript}
+        </div>
+    </div>
+    <div class="option">
+        <button>Edit</button>
+        <button>Delete</button>
+    </div>
+ </div>`
+ 
 
+    parent.append(h1);
 
-    // pengambilan data dari checkbox
-
-   console.log(files);
-
+   console.log(schema);
+     
+    
+   
 })
+
